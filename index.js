5a891ca6bea23bd7a5788083c658921103f0c95b
Rick Carlino <rick.carlino@gmail.com>
2017-05-02 21:32:44 -0500

    WIP: MQTTjs issues.

--- index.js
+++ index.js
@@ -1,4 +1,53 @@
 let irc = require("irc");
-let axios = require("axios");
-let farmbot = requre("farmbot");
+let axios = require("axios").default;
+let secret = require("./secret.json");
+let Farmbot = require("farmbot").Farmbot;
 
+// Node JS workaround:
+// SEE https://github.com/FarmBot/farmbot-js/issues/33
+global.atob = require("atob");
+
+// SEE: https://github.com/FarmBot/Farmbot-Web-API#generating-an-api-token
+let API_URL = "https://my.farmbot.io/api/tokens";
+
+// SEE: https://github.com/martynsmith/node-irc#basic-usage
+let IRC_SERVER = "irc.freenode.net";
+let IRC_NICK = "potato_tornado";
+let IRC_CONFIG = { channels: ['#farmbot_example'] };
+
+// STEP ONE: Get an API token
+axios
+  .post(API_URL, secret)
+  .then(function (resp) {
+    let token = resp.data.token.encoded;
+    console.log(`Got API Token...`);
+    console.dir(resp.data.token.unencoded)
+    // STEP TWO: Connect to your FarmBot:
+    let bot = new Farmbot({ token: token, secure: true });
+
+    bot
+      .connect()
+      .then(function () {
+        console.log("CONNECTED TO BOT!")
+        // STEP THREE: Connect to IRC
+        var client = new irc.Client(IRC_SERVER, IRC_NICK, IRC_CONFIG);
+        client.addListener('message', function (from, to, message) {
+          if (message.startsWith("up")) {
+            bot
+              .moveRelative({ x: 7, y: 0, z: 0 })
+              .then(() => console.log("MOVING!!!"))
+              .catch(() => console.log("CANT MOVE T_T"));
+          }
+          console.log("GOT: " + message);
+        });
+      })
+      .catch(function (e) {
+        console.log("FAILED TO CNNECT TO BOT!!");
+        console.dir(e);
+        console.dir(bot._state);
+      })
+  })
+  .catch(function (err) {
+    console.log("BAD PASSWORD OR EMAIL!");
+    console.dir(err);
+  });
--- readme.md
+++ readme.md
@@ -6,5 +6,17 @@ Control FarmBot via IRC
 
 1. Copy `secret.example.json` to `secret.json`.
 2. Change `email` and `password` in `secret.json`*
+2. Change IRC_SERVER and IRC\_NICK in `index.js` to something better.
 3. (first time only) `npm install`
 4. `npm start`
+
+# Usage
+
+1. Join `#farmbot_example` on `irc.freenode.net` using an IRC client.
+2. Look for `potato_tornado` (you can change this).
+3. say `up` in the channel.
+4. Z axis should move up by 7 steps.
+5. EXTRA CREDIT: Add commands of your own!
+6. EXTRA CREDIT: Refactor the code into multiple files.
+
+
